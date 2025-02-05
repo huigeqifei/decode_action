d2212be1a826faa6a29fea2e3d45eccf2861472d
action <action@github.com>
2025-02-05 07:30:38 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,10 +1,29 @@
-//Wed Feb 05 2025 07:29:45 GMT+0000 (Coordinated Universal Time)
+//Wed Feb 05 2025 07:30:37 GMT+0000 (Coordinated Universal Time)
 //Base:https://github.com/echo094/decode-js
 //Modify:https://github.com/smallfawn/decode_action
-function dc() {
-  var _0x16d787 = sessionStorage.getItem(TOKEN);
-  var _0xe6d400 = sessionStorage.getItem(STR);
-  _0x16d787 = _0x16d787 + "&str=" + _0xe6d400;
-  var _0x4b3a90 = "/uaction/abc?token=" + _0x16d787;
-  return _0x4b3a90;
+function abc() {
+  var _0xe3ab21 = dc();
+  $.ajax({
+    "type": "GET",
+    "url": originPath + _0xe3ab21,
+    "timeout": 10000,
+    "success": function (_0x1b0a10) {
+      {
+        if (_0x1b0a10.code == 200) {
+          if (_0x1b0a10.data.code == 200) sessionStorage.setItem("ffb", _0x1b0a10.data.a), sessionStorage.setItem(STR, _0x1b0a10.data.str), getTask();else {
+            if (302 == _0x1b0a10.data.code) titleText2.text("请进入公众号进行文章阅读"), $("#myModal2").show(), timerTask2();else {
+              titleText2.text("任务失效了！请返回");
+              $("#myModal2").show();
+              timerTask2();
+            }
+          }
+        } else titleText2.text("访问出错了，请返回重试。"), $("#myModal2").show(), timerTask2();
+      }
+    },
+    "error": function (_0xca068c, _0x3e1f14) {
+      titleText2.text("请求超时了！返回重试");
+      $("#myModal2").show();
+      timerTask2();
+    }
+  });
 }

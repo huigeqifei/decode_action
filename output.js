99eb7aba01914e5f9e29eec66f325f417778fced
action <action@github.com>
2025-02-05 07:27:51 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,12 +1,19 @@
-//Wed Feb 05 2025 07:25:42 GMT+0000 (Coordinated Universal Time)
+//Wed Feb 05 2025 07:27:51 GMT+0000 (Coordinated Universal Time)
 //Base:https://github.com/echo094/decode-js
 //Modify:https://github.com/smallfawn/decode_action
-function lj() {
-  var _0xb5313b = sessionStorage.getItem(TOKEN);
-  var _0x3712f8 = sessionStorage.getItem(START_NUM);
-  var _0x4037fd = sessionStorage.getItem(STR);
-  var _0x3c914a = ee();
-  _0xb5313b = _0xb5313b + "&startNumber=" + _0x3712f8 + "&str=" + _0x4037fd + "&keys=" + _0x3c914a + "&ds=";
-  var _0x554c2f = "/uaction/edbbcdc/getArticleListkk?token=" + _0xb5313b;
-  return _0x554c2f;
+function getTask() {
+  var _0xebfc2b = lj();
+  $.ajax({
+    "type": "GET",
+    "url": originPath + _0xebfc2b,
+    "timeout": 10000,
+    "success": function (_0x1d14c1) {
+      result(_0x1d14c1);
+    },
+    "error": function (_0x2285a1, _0x3a07d5) {
+      titleText2.text("请求超时了！返回重试");
+      $("#myModal2").show();
+      timerTask2();
+    }
+  });
 }

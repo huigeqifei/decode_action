334bddfbf376dace7f95c74d0b14e4861401ed1a
action <action@github.com>
2025-02-05 07:28:49 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,19 +1,21 @@
-//Wed Feb 05 2025 07:27:51 GMT+0000 (Coordinated Universal Time)
+//Wed Feb 05 2025 07:28:49 GMT+0000 (Coordinated Universal Time)
 //Base:https://github.com/echo094/decode-js
 //Modify:https://github.com/smallfawn/decode_action
-function getTask() {
-  var _0xebfc2b = lj();
-  $.ajax({
-    "type": "GET",
-    "url": originPath + _0xebfc2b,
-    "timeout": 10000,
-    "success": function (_0x1d14c1) {
-      result(_0x1d14c1);
-    },
-    "error": function (_0x2285a1, _0x3a07d5) {
-      titleText2.text("请求超时了！返回重试");
-      $("#myModal2").show();
-      timerTask2();
-    }
-  });
+function result22(_0x5b0328) {
+  if (_0x5b0328.data.code == 501) {
+    titleText2.text("非法请求！请返回");
+    $("#myModal2").show();
+    timerTask2();
+    return;
+  }
+  if (_0x5b0328.data.code == 150) {
+    $("#myModal").show();
+    timerTask();
+    return;
+  }
+  if (_0x5b0328.data.code == 300) {
+    overTask("已完成", "任务已完成，请返回。", "任务经完成！请返回");
+    return;
+  }
+  _0x5b0328.data.code == 200 && (sessionStorage.setItem(START_NUM, _0x5b0328.data.startNum), sessionStorage.setItem(END_NUM, _0x5b0328.data.endNum), sessionStorage.setItem("ffb", _0x5b0328.data.ffb), readArt(_0x5b0328.data.url));
 }

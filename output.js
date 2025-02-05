107776496d775e34eb49c7aec4935c66a0ec64ab
action <action@github.com>
2025-02-05 07:29:45 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,21 +1,10 @@
-//Wed Feb 05 2025 07:28:49 GMT+0000 (Coordinated Universal Time)
+//Wed Feb 05 2025 07:29:45 GMT+0000 (Coordinated Universal Time)
 //Base:https://github.com/echo094/decode-js
 //Modify:https://github.com/smallfawn/decode_action
-function result22(_0x5b0328) {
-  if (_0x5b0328.data.code == 501) {
-    titleText2.text("非法请求！请返回");
-    $("#myModal2").show();
-    timerTask2();
-    return;
-  }
-  if (_0x5b0328.data.code == 150) {
-    $("#myModal").show();
-    timerTask();
-    return;
-  }
-  if (_0x5b0328.data.code == 300) {
-    overTask("已完成", "任务已完成，请返回。", "任务经完成！请返回");
-    return;
-  }
-  _0x5b0328.data.code == 200 && (sessionStorage.setItem(START_NUM, _0x5b0328.data.startNum), sessionStorage.setItem(END_NUM, _0x5b0328.data.endNum), sessionStorage.setItem("ffb", _0x5b0328.data.ffb), readArt(_0x5b0328.data.url));
+function dc() {
+  var _0x16d787 = sessionStorage.getItem(TOKEN);
+  var _0xe6d400 = sessionStorage.getItem(STR);
+  _0x16d787 = _0x16d787 + "&str=" + _0xe6d400;
+  var _0x4b3a90 = "/uaction/abc?token=" + _0x16d787;
+  return _0x4b3a90;
 }
